293d8b1abb40f55ab9024fec8858d0b43dc7e09d
PetarPisarski <peter.pisarski@outlook.com>
2026-03-09 15:09:51 -0700

    First Week Commit

---
+++ Week 02/Solutions/Problem 13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    int numberOne =   0;
+    int numberTwo =   0;
+    int numberThree =   0;
+
+
+    puts("Enter the Number 1: \n");
+    scanf("%d", &numberOne);
+
+    puts("Enter the Number 2: \n");
+    scanf("%d", &numberTwo);
+
+    puts("Enter the Number 3: \n");
+    scanf("%d", &numberThree);
+
+
+    int countPositive = (numberOne > 0) + (numberTwo > 0) + (numberThree > 0);
+    int countNegative = (numberOne < 0) + (numberTwo < 0) + (numberThree < 0);
+
+    printf("The count of the positive numbers is: %d\n",    countPositive);
+    printf("The count of the negative numbers is: %d\n",    countNegative);
+
+    return 0;
+}
